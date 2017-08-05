ff1c8c13476938fec106b3263f01e0eb42c2ecb1
Serhii <dharm.serg@gmail.com>
2017-08-05 12:45:21 +0300

    Updated logic. Added features to check mining stats.

--- controllers/MessageColector.js
+++ controllers/MessageColector.js
@@ -5,6 +5,8 @@ let htmlparser = require("htmlparser2");
 let select = require('soupselect').select;
 const random = require('node-random-number');
 let _ = require('lodash');
+let moment = require('moment');
+const BOT_CONFIGS = require('../configBOT');
 
 let excusePath = `/get-excuse/367`;
 
@@ -35,6 +37,9 @@ class MessageColector {
     
     if (this.match[0] === '/get_excuse') this.getExecuse()
     else if (this.match[0] === '/get_joke') this.getJoke()
+    else if (this.match[0] === '/m') this.getMiningEthermineStats('serg')
+    else if (this.match[0] === '/ms') this.getMiningEthermineStats('sem')
+    else if (this.match[0] === '/autoGPUAlert') this.lowHighTemperatureAlert();
     else {
       this.bot.sendSticker(this.chatId, 'CAADAgADEgUAAvoLtgjV7M7AFx5kYwI');
       this.bot.sendMessage(this.chatId, 'Что я могу вам предоставить:', this.inlineKeyboard(['get_excuse', 'get_joke'], true));
@@ -83,6 +88,121 @@ class MessageColector {
       } catch (error) {
         that.bot.sendMessage(that.chatId, 'Херня дело, попробуй позже..');
       }     
+    });
+  }
+
+  getMiningEthermineStats(option) {
+    let that = this;
+    var Request = unirest.get(`https://ethermine.org/api/miner_new/${BOT_CONFIGS.ethermineIDS[option]}`);
+
+    Request.end(function (response) {
+      let nextPayoutHours = (((+response.body.settings.minPayout - +response.body.unpaid)/ 1000000000000000000)/(+response.body.ethPerMin * 60)).toFixed(1);
+      nextPayoutHours = Math.round(nextPayoutHours);
+      let payoutDay = moment().add(nextPayoutHours, 'h');
+
+      var  msg = `
+      <code>Address: </code><b>${response.body.address}</b>\n
+      <code>Unpaid ethereum: </code><b>${ (+response.body.unpaid / 1000000000000000000).toFixed(5)}</b>\n
+      <b>===HASHRATE===</b>\n
+      <code>Current hashrate: </code><b>${response.body.hashRate}</b>\n
+      <code>Average hashrate: </code><b>${(+response.body.avgHashrate / 1000000).toFixed(5)} MH/s</b>\n
+      <code>Reported hashrate: </code><b>${response.body.reportedHashRate}</b>\n
+      <b>===ETHER PER===</b>\n
+      <code>Ether per minute: </code><b>${+response.body.ethPerMin.toFixed(6)}</b>\n
+      <code>Ether per day: </code><b>${(+response.body.ethPerMin * 1440).toFixed(5)}</b>\n
+      <code>Ether per week: </code><b>${(+response.body.ethPerMin * 7 * 1440).toFixed(5)}</b>\n
+      <b>===USD PER===</b>\n
+      <code>USD per minute: </code><b>${+response.body.usdPerMin.toFixed(5)} USD</b> \n
+      <code>USD per day: </code><b>${(+response.body.usdPerMin * 1440).toFixed(5)} USD</b> \n
+      <code>USD per week: </code><b>${(+response.body.usdPerMin * 7 * 1440).toFixed(5)} USD</b> \n
+      <b>===NEAREST PAYOUT===</b>\n
+      ${payoutDay.format("dddd, MMMM Do YYYY, h:mm:ss a")}
+
+      <b>===ETHERMINE WEB SITE===</b>\n
+      <a href="https://ethermine.org/miners/${BOT_CONFIGS.ethermineIDS[option]}">STATS ON WEB-site</a>
+
+      <b>===DONATE HERE===</b>\n
+      ETH: 0x7fd34080918b7a48f797364c1016ef0d4136262a \n
+      `;
+
+      that.bot.sendMessage(that.chatId, msg, {parse_mode:'html', disable_web_page_preview:true});
+      
+      if (option === 'serg') {
+        that.getGPUTemperature();
+      }
+    });
+  }
+
+  getGPUTemperature() {
+    let that = this;
+    var Request = unirest.get(BOT_CONFIGS.rigAdress);
+    Request.end(function (response) {
+      var handler = new htmlparser.DomHandler(function(err, dom) {
+        let temperature = select(dom, 'body')[0].children[0].data;
+        temperature = JSON.parse(temperature).result[6];
+        temperature = temperature.split(';')
+
+        let temperatureString = '';
+        
+        _.forEach(temperature, (v,i) => {
+          if (i%2 === 0) {
+            temperatureString = temperatureString + `<code>GPU-${i === 0 ? 0 : i/2}</code> \n <strong>t - ${v}C  `;
+          }
+          else if (i%2 === 1) {
+            temperatureString = temperatureString + `fan - ${v}%</strong> \n`;
+          }
+        });
+
+        that.bot.sendMessage(that.chatId, temperatureString, {parse_mode:'html', disable_web_page_preview:true});             
+      });
+      
+      new htmlparser.Parser(handler).parseComplete(response.body)
+    });
+  }
+
+  lowHighTemperatureAlert() {
+    let that = this;
+    var Request = unirest.get(BOT_CONFIGS.rigAdress);
+    Request.end(function (response) {
+      var handler = new htmlparser.DomHandler(function(err, dom) {
+        if (err) {
+          that.bot.sendMessage(that.chatId, 'Have no access to the local statistic server.');
+        }
+        else {
+          let temperature = select(dom, 'body')[0].children[0].data;
+          temperature = JSON.parse(temperature).result[6];
+          temperature = temperature.split(';')
+
+          let isThereAnAlertMessage = '';
+          let isThereAnAlert = false;
+          
+          _.forEach(temperature, (v,i) => {
+            // If high temperature
+            if (i%2 === 0) {
+              let isToLowTemperature = !v || +v < BOT_CONFIGS.GPULowTemperature;
+              let isToHighTemperature = +v > BOT_CONFIGS.GPUTLimit;
+              if (isToLowTemperature || isToHighTemperature) {
+                isThereAnAlertMessage = isThereAnAlertMessage + `<strong>Danger temperature.</strong> <i>Limit ${BOT_CONFIGS.GPUTLimit}C</i>\n<code>GPU-${i === 0 ? 0 : i/2}</code> \n <strong>t - ${v}C</strong> \n`;
+              }
+
+              isThereAnAlert = isToLowTemperature || isToHighTemperature || temperature.length < BOT_CONFIGS.GPUCount*2;
+            }
+          });
+
+          // if one of GPU went down
+          if (temperature.length < BOT_CONFIGS.GPUCount*2) {
+            isThereAnAlertMessage = isThereAnAlertMessage + `<code>Some of your GPU went down!</code>`;
+          }
+
+          //Send alert if it's reasons
+          if (isThereAnAlert) {
+            that.bot.sendMessage(that.chatId, isThereAnAlertMessage, {parse_mode:'html', disable_web_page_preview:true});
+          }             
+        }
+        
+      });
+      
+      new htmlparser.Parser(handler).parseComplete(response.body)
     });
   }
 }
--- start.js
+++ start.js
@@ -1,22 +1,29 @@
 'use strict';
-
+const BOT_CONFIGS = require('./configBOT');
 const TelegramBot = require('node-telegram-bot-api');
 const MessageColector = require('./controllers/MessageColector');
 let _ = require('lodash');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '387116379:AAGHOhcMN4DH0RzEagC3hMmLj7msVfGGlHk';
+const token = BOT_CONFIGS.telegrammBotToken;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on('callback_query', function(msg) {
-    var match = /\/get_excuse (.+)/.exec(msg.data) || /\/get_joke (.+)/.exec(msg.data);
+    var match = /\/get_excuse (.+)/.exec(msg.data) || /\/get_joke (.+)/.exec(msg.data) || /\/m (.+)/.exec(msg.data) || /\/ms (.+)/.exec(msg.data);
 
     // To define chat.id
-    // _.set(msg, 'chat.id', match[1])
+    _.set(msg, 'chat.id', match[1]);
 
     if (match) new MessageColector(msg, [msg.data.split(' ')[0]], bot)
 });
 // IF MESSAGE MATCHES "[whatever]"
 bot.onText(/(.+)/, (msg, match) => new MessageColector(msg, match, bot));
+
+
+
+setInterval(function() {
+  new MessageColector({chat:{id: BOT_CONFIGS.myChatId}}, ['/autoGPUAlert'], bot);
+}, 5000);
+//}, 1800000);
